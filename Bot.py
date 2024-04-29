ace6134b7d1806599016dd9113db2cde4c8e6be3
a40bit <165264568+a40bit@users.noreply.github.com>
2024-04-29 21:21:28 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
-BOT_TOKEN = '7173156810:AAFovgNeUCM_Voqk0nvJQo1kUbLI66VEzng'
+BOT_TOKEN = 
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
